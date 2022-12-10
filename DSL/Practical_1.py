c5f4d995aba8b072c1092cc37326e915021dffc9
Anujchobe <98049418+Anujchobe@users.noreply.github.com>
2022-12-10 12:19:06 +0530

    Marks with highest frequency not calculated
    Check and accept pull request

--- DSL/Practical_1.py
+++ DSL/Practical_1.py
@@ -1,77 +1,96 @@
-'''
+# Write a Python program to store marks scored in subject “Fundamental of Data
+# Structure” by N students in the class. Write functions to compute following:
+# a) The average score of class
+# b) Highest score and lowest score of class
+# c) Count of students who were absent for the test
+# d) Display mark with highest frequency
 
-Assignemt No: 01 (A-01)
-Assignemt Title: Write a Python program to store marks scored in subject “Fundamental
-⦁ The average score of class
-⦁ Highest score and lowest score of class
-⦁ Count of students who were absent for the test
-⦁ Display mark with highest frequency
-'''
-def marksentry(A):
-    n=int(input("Enter the number of Student: "))
-    for i in range (n):
-        marks=input("Enter marks of roll no. %d student:"%(i+1))
-        if marks=="AB":
-            marks=-1
-        A.append(int(marks))
 
-    print(*A,sep=",")
-    
-    
-def showmark(A):
-    for i in range (len(A)):
-        if A[i]==-1:
-            print("Marks of roll no. %d: Absent "%(i+1))
-        if A[i]!=-1:
-            print("Marks of roll no. %d:"%(i+1),A[i])
-            
-            
-def minmax(A):
-    mini=31
-    maxi=-1
-    for i in range (len(A)):
-        if A[i]>maxi:
-            maxi=A[i]
-            
-        if A[i]<mini and A[i]!=-1:
-            mini=A[i]
-    print("Maximum marks:",maxi)
-    print("Minimum marks:",mini)
-    
-def average(A):
-    summ=0
-    count=0
-    for i in range (len(A)):
-        if A[i]!=-1:
-            count=count+1
-            summ=summ+A[i]
-    average=summ/count
-    print("Averange marks of student:",average)
-    
+def getmarks(mrklist):
+    n=int(input("Enter total number of students"))
+
+    #input
+    for i in range(n):
+        mrk=(input("Enter marks obtained or 'ab' if student is absent"))
+        if mrk!="ab":
+            mrklist.append(int(mrk))
+        else:
+            mrklist.append(mrk)    
+
+#average
+def avg(mrklist):
+    sum=0
+    ctr=0
+    for mk in mrklist:
+        if mk!="ab":
+            sum+=mk
+            ctr=ctr+1
+    print(f"Average score of class = {sum/ctr}")  
+
+#highest and lowest score
+def highlow(mrklist):
+    min=0
+    max=0
+    ct=0
+    for i in mrklist:        
+        if i!="ab":
+            if ct==0 :
+                min=i
+                ct+=1
+            if min>i:
+                min=i
+            if max<i:
+                max=i    
+    print(f"Highest score of class = {max} and Lowest score of class= {min}")        
+
+#absent
+def absent(mrklist):
+    ab=0
+    for i in mrklist:
+        if i=="ab":
+            ab+=1
+    print(f"Number of students that were absent = {ab}")
+
+#Frequency
+def frequency(mrklist):
+    freq={}
+    mfreq=0
+    for i in mrklist:
+        if i !="ab":
+            if i!=freq.keys():
+                freq[i]=1
+            else:
+                freq[i]+=1
+
+    for i in freq:
+        if mfreq<freq[i]:
+                mfreq=freq[i]        
+    key=list(freq.keys()) 
+    val=list(freq.values())                     
+    pos=val.index(mfreq)
+    print("Marks with highest Frequency = ",key[pos])
+
 def main():
-    lst=[]
+    mrklist=[]
     while True:
-        print("1. Enter 1 for marks entry")
-        print("2. Enter 2 for average marks")
-        print("3. Enter 3 for show marks")
-        print("4. Enter 4 for minimum and maximum marks")
-        print("5. Stop")
-        ch=int(input("Enter your choice : "))
+        print("Enter '1' for entering marks")
+        print("Enter '2' to display average marks")
+        print("Enter '3' to diaplay highest and lowest marks ")
+        print("Enter '4' to display number of absent students")
+        print("Enter '5' to display marks with highest frequency")
+        print("Enter '6' to STOP")
+        ch=int(input("Enter your choice here: "))
         if ch==1:
-            marksentry(lst)
-        elif ch==2:
-            average(lst)
-        elif ch==3:
-            showmark(lst)
-        elif ch==4:
-            minmax(lst)
-        elif ch==5:
-            print("Thank you")
+            getmarks(mrklist)
+        if ch==2:
+            avg(mrklist)    
+        if ch==3:
+            highlow(mrklist)
+        if ch==4:
+            absent(mrklist)    
+        if ch==5:
+            frequency(mrklist)
+        if ch==6:
+            print("Thank You!")
             break
-        else :
-            print("Please enter valid choice")
-main()
-    
-    
-    
-    
+main()        
