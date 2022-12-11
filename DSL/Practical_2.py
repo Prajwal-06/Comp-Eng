d500139d6b5853c286d3a112e918ada345c77e23
Prajwal-06 <113961048+Prajwal-06@users.noreply.github.com>
2022-12-11 17:40:40 +0530

    Done

--- DSL/Practical_2.py
+++ DSL/Practical_2.py
@@ -1,7 +1,7 @@
 # Write a Python program that computes the net amount of a bank account based a
 # transaction log from console input. The transaction log format is shown as following: D
 # 100 W 200 (Withdrawal is not allowed if balance is going negative. Write functions for
-# withdraw and deposit) D means deposit while W means withdrawal.
+# withdraw and deposit) D means deposit while W means withdrawal
 # Suppose the following input is supplied to the program:
 # D 300
 # D 300
